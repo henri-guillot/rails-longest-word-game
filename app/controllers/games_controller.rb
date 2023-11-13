fe394e3418c3f7d4204d077855d9aa51c989489a
henri guillot <henri.guillot@hotmail.com>
2023-11-13 21:45:39 +0100

    done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,9 +7,17 @@ class GamesController < ApplicationController
     @random_letters = ('a'..'z').to_a.sample(10)
   end
 
-  def score
+  def score()
+    @answer=""
     p params[:word]
     p english_word?(params[:word])
+    if english_word?(params[:word]) & letters_contained?(params[:word], params[:random_letters])
+      @answer = "congrats!"
+    elsif english_word?(params[:word])
+      @answer = "you must include only given letters!"
+    else
+      @answer = "this is not an english word.."
+    end
     p letters_contained?(params[:word], params[:random_letters])
   end
 
